903e4ec056c74100b85940388acc30595e0707d9
Ayan Ali <ayan_ali2004@hotmail.com>
2026-02-25 03:59:34 +0000

    began free list integration

--- main.cpp
+++ main.cpp
@@ -6,13 +6,33 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+static constexpr size_t MMAP_THRESHOLD = 128000;
+
+struct Block {
+    size_t size;
+    bool is_free;
+    Block* next;
+};
+
 struct Allocator {
+    Block* free_list; // start of heap
+    size_t available_bytes;
+
     void* allocate(size_t size) {
-        // int fd = open("/dev/zero", O_RDWR);
-        // if (fd == -1) {
-        //     std::cerr << "Failed to open device";
-        // }
-        void* mem = (void*)mmap(
+        if (size < MMAP_THRESHOLD && size <= available_bytes) {
+            // get a block from the free list
+            Block* curr = free_list;
+            while (curr) {
+                // look for the first free block that contains enough size to satisfy the request
+                if (curr->is_free && curr->size >= size) {
+                    curr->is_free = false;
+                    available_bytes -= curr->size;
+                    return (void*)(curr + 1);
+                }
+                curr = curr->next;
+            }
+        }
+        void* mem = mmap(
             // TODO: consider MAP_NORESERVE, MAP_HUGETLB, MAP_LOCKED, MAP_POPULATE
             nullptr, size, 
             PROT_READ | PROT_WRITE, 
@@ -21,10 +41,13 @@ struct Allocator {
         return mem;
     }
 
+    void deallocate_block(Block* ptr) {
+        ptr->is_free = true;
+    }
+
     template <typename T>
-    int deallocate(T* ptr, size_t size) {
-        int ok = munmap(ptr, size);
-        return ok;
+    void deallocate_large(T* ptr) {
+        munmap(void *addr, size_t len);
     }
 };
 
