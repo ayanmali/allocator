213a55ae3edef96812bd076642037839c48abecd
Ayan Ali <ayan_ali2004@hotmail.com>
2026-02-28 16:59:45 +0000

    fixing bugs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <cstddef>
+#include <cstdint>
 #include <cstdio>
-#include <iostream>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -12,16 +12,33 @@ struct Block {
     // Block header
     size_t size;
     bool is_free;
+    bool mapped;
     Block* next;
     std::byte memory[];
 };
 
+static constexpr size_t header_size() {
+    return offsetof(Block, memory);
+}
+
 struct Allocator {
+    static constexpr size_t INITIAL_CHUNK = 4096;
     Block* free_list; // start of heap
     size_t available_bytes;
 
-    static constexpr size_t get_header_size() {
-        return sizeof(size_t) + sizeof(bool) + sizeof(Block*);
+    Allocator() {
+        void* base = sbrk(INITIAL_CHUNK);
+        // error
+        if (base == (void*)-1) {
+            free_list = nullptr;
+            available_bytes = 0;
+            return;
+        }
+        free_list = static_cast<Block*>(base);
+        free_list->is_free = true;
+        free_list->next = nullptr;
+        free_list->size = INITIAL_CHUNK - header_size();
+        available_bytes = free_list->size;
     }
 
     void* allocate(size_t size) {
@@ -41,17 +58,24 @@ struct Allocator {
 
         // allocate additional memory via sbrk
         if (size < REQ_THRESHOLD) {
-            Block* block = (Block*)sbrk(size);
+            void* base = sbrk(size + header_size());
+            if (base == (void*)-1) {
+                return (void*)-1;
+            }
+            Block* block = static_cast<Block*>(base);
             // add to free list
             Block* curr = free_list;
             while (curr && curr->next) {
                 curr = curr->next;
             }
             curr->next = block;
+
             block->is_free = false;
             block->size = size;
+            block->next = nullptr;
+            block->mapped = false;
             
-            return block + get_header_size();
+            return block + header_size();
         }
 
         // for large requests, allocate via mmap
@@ -61,33 +85,66 @@ struct Allocator {
             curr = curr->next;
         }
 
-        Block* new_block = (Block*) mmap(
+        void* base = mmap(
             // TODO: consider MAP_NORESERVE, MAP_HUGETLB, MAP_LOCKED, MAP_POPULATE
             curr->next, size,
             PROT_READ | PROT_WRITE, 
             MAP_ANONYMOUS | MAP_PRIVATE,
              -1, 0);
-
+        
+        if (base == MAP_FAILED) {
+            return MAP_FAILED;
+        }
+        Block* new_block = static_cast<Block*>(base);
         curr->next = new_block;
+        available_bytes -= curr->size;
         
         // adjust block metadata
         new_block->is_free = false;
-        new_block->size = size;
+        new_block->size = size - header_size();
+        new_block->next = nullptr;
+        new_block->mapped = true;
 
-        return new_block + get_header_size();
+        return new_block + header_size();
     }
 
+    /*
+    Deallocation involves shrinking the program break (to free memory allocated with sbrk)
+    and calling `munmap` on mapped memory.
+    */
     template <typename T>
-    void deallocate(T* ptr) {
-        // ptr is the start of the Block's memory array
-        Block* block = (Block*) ptr - get_header_size();
+    uint8_t deallocate(T* ptr) {
+        // ptr point to the start of the Block's memory
+        Block* block = static_cast<Block*>(ptr - header_size());
+        return deallocate(block);
+    }
+
+    uint8_t deallocate(Block* block) {
+        if (block->mapped) {
+            int ok = munmap(block->memory, header_size() + block->size);
+            if (ok == -1) {
+                return -1;
+            }
+        } 
+        else {
+            void* base = sbrk(block->size * -1);
+            if (base == (void*)-1) {
+                return -1;
+            }
+        }
         block->is_free = true;
+        available_bytes += block->size;
+        return 0;
     }
 
 };
 
 int main() {
     Allocator allocator{};
-    int* n = static_cast<int*>(allocator.allocate(100));
+    int* n = (int*)(allocator.allocate(100));
+    Block* b = (Block*) (n - header_size());
+    // std::cout << "Is free: " << b->is_free << "\n";
+    // std::cout << "Block size: " << b->size << "\n";
+    allocator.deallocate(b);
     return 0;
 }
