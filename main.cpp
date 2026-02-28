b3925654aa905d7fbdfd1191f3a13ba78e911053
Ayan Ali <ayan_ali2004@hotmail.com>
2026-02-28 05:03:28 +0000

    refactored allocation function

--- main.cpp
+++ main.cpp
@@ -6,20 +6,26 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-static constexpr size_t MMAP_THRESHOLD = 128000;
+static constexpr size_t REQ_THRESHOLD = 128000;
 
 struct Block {
+    // Block header
     size_t size;
     bool is_free;
     Block* next;
+    std::byte memory[];
 };
 
 struct Allocator {
     Block* free_list; // start of heap
     size_t available_bytes;
 
+    static constexpr size_t get_header_size() {
+        return sizeof(size_t) + sizeof(bool) + sizeof(Block*);
+    }
+
     void* allocate(size_t size) {
-        if (size < MMAP_THRESHOLD && size <= available_bytes) {
+        if (size <= available_bytes) {
             // get a block from the free list
             Block* curr = free_list;
             while (curr) {
@@ -27,33 +33,58 @@ struct Allocator {
                 if (curr->is_free && curr->size >= size) {
                     curr->is_free = false;
                     available_bytes -= curr->size;
-                    return (void*)(curr + 1);
+                    return curr->memory;
                 }
                 curr = curr->next;
             }
         }
-        void* mem = mmap(
+
+        // allocate additional memory via sbrk
+        if (size < REQ_THRESHOLD) {
+            Block* block = (Block*)sbrk(size);
+            // add to free list
+            Block* curr = free_list;
+            while (curr && curr->next) {
+                curr = curr->next;
+            }
+            curr->next = block;
+            block->is_free = false;
+            block->size = size;
+            
+            return block + get_header_size();
+        }
+
+        // for large requests, allocate via mmap
+        // allocate a new block and add it to the list
+        Block* curr = free_list;
+        while (curr && curr->next) {
+            curr = curr->next;
+        }
+
+        Block* new_block = (Block*) mmap(
             // TODO: consider MAP_NORESERVE, MAP_HUGETLB, MAP_LOCKED, MAP_POPULATE
-            nullptr, size, 
+            curr->next, size,
             PROT_READ | PROT_WRITE, 
             MAP_ANONYMOUS | MAP_PRIVATE,
              -1, 0);
-        return mem;
+
+        curr->next = new_block;
+        
+        // adjust block metadata
+        new_block->is_free = false;
+        new_block->size = size;
+
+        return new_block + get_header_size();
     }
 
-    void deallocate_block(Block* ptr) {
+    void deallocate(Block* ptr) {
         ptr->is_free = true;
     }
 
-    template <typename T>
-    void deallocate_large(T* ptr) {
-        munmap(void *addr, size_t len);
-    }
 };
 
 int main() {
     Allocator allocator{};
     int* n = static_cast<int*>(allocator.allocate(100));
-    allocator.deallocate(n, 100);
     return 0;
 }
